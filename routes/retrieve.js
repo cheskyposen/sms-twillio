53b8cd516e84aabc8ccc7d21dc93472e0e158352
shraga posen <shragaposen@gmail.com>
2019-06-05 20:43:26 -0400

    debbbugging

--- routes/retrieve.js
+++ routes/retrieve.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const client = require('twilio')(process.env.ACCOUNT_SID, process.env.AUTH_TOKEN);
 
 /* GET messages. */
-router.get('/', async function(req, res) {
+router.get('/', async function(req, res, next) {
     let messages = {};
     await client.messages.list({limit: 20})
         .then(results => messages = results);
--- routes/send.js
+++ routes/send.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const client = require('twilio')(process.env.ACCOUNT_SID, process.env.AUTH_TOKEN);
 
 // /* POST sms msg. */
-router.post('/', async function(req, res) {
+router.post('/', async function(req, res, next) {
     let response = {};
     await client.messages
         .create({from: process.env.FROM, body: req.body.msg, to: req.body.tell})
